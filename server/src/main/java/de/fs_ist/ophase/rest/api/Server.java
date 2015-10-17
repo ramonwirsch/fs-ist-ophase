4e2ed4ea0b33c5818759b0cec9f99315e7a81f29
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2015-10-17 18:32:36 +0200

    * running in wildfly incl. JPA * using slf4j log mapping into wildfly

--- server/src/main/java/de/fs_ist/ophase/rest/api/Server.java
+++ server/src/main/java/de/fs_ist/ophase/rest/api/Server.java
@@ -1,7 +1,14 @@
 package de.fs_ist.ophase.rest.api;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by ramon on 04.10.2015.
@@ -9,9 +16,25 @@ import javax.ws.rs.Path;
 @Path("server")
 public class Server {
 
+	private static final Logger log = LoggerFactory.getLogger(Server.class);
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
     @GET
     @Path("status")
     public String getStatus() {
+		log.info("ok");
         return "Up and running";
+    }
+
+    @GET
+    @Path("jpatest")
+    public String getJpaTest() {
+        emf = Persistence.createEntityManagerFactory("de.fs_ist.ophase");
+		log.info("EMF: {}", emf);
+        EntityManager em = emf.createEntityManager();
+		log.info("EM: {}", em);
+        return em.toString();
     }
 }
